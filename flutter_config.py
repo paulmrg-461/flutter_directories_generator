e32cffef25f4d8db36896ec1af7d00acf809310a
paulmrg-461 <paulmrg461@gmail.com>
2023-02-03 15:57:32 -0500

    Fix: creation Flutter project

--- flutter_config.py
+++ flutter_config.py
@@ -3,4 +3,4 @@ import sys
 
 temp_file_name = sys.argv[1]
 
-os.system(f'cd ../ flutter create {temp_file_name} && cd ./{temp_file_name}/lib && mkdir domain data ui device && cd domain && mkdir entities bloc repositories services && cd ../data/ &&  mkdir repositories services && cd ../ui/ && mkdir screens shared themes routes')
+os.system(f'flutter create {temp_file_name} && cd ./{temp_file_name}/lib && mkdir domain data ui device && cd domain && mkdir entities bloc repositories services && cd ../data/ &&  mkdir repositories services && cd ../ui/ && mkdir screens shared themes routes')
