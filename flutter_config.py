76425629b3b90b4fc51b4ddc0f8f9c08678f1404
paulmrg-461 <paulmrg461@gmail.com>
2023-05-16 08:25:32 -0500

    Feat: Added clean architecture directories

--- flutter_config.py
+++ flutter_config.py
@@ -3,4 +3,4 @@ import sys
 
 temp_file_name = sys.argv[1]
 
-os.system(f'flutter create {temp_file_name} && cd ./{temp_file_name}/lib && mkdir domain data ui device && cd domain && mkdir entities bloc repositories services && cd ../data/ &&  mkdir repositories services && cd ../ui/ && mkdir screens shared themes routes')
+os.system(f'cd ../ && flutter create {temp_file_name} && cd ./{temp_file_name}/lib && mkdir domain infrastructure presentation config core && cd domain && mkdir entities repositories datasources && cd ../infrastructure/ &&  mkdir datasources repositories models mappers && cd ../presentation/ && mkdir providers screens views widgets && cd ../config/ && mkdir constants router theme && touch config.dart && cd ./constants/ && touch environment.dart')
